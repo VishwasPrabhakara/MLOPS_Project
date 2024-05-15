aa111a87e28d8b2e4e14b251cfb7eb5ffeb6fec8
Vishwas <vishwas.msismpl2023@learner.manipal.edu>
2024-05-16 00:31:13 +0530

    Deployment Configuration AWS

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages,setup
 from typing import List
 
-def get_requirements(file_path: str)->List[str]:
-    '''i
-    This function will return the list of requirements
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
     '''
-    requirements = []
-    with open(file_path, 'r') as f:
-        requirements = f.readlines()
-        [req.replace('\n', '') for req in requirements] 
-        if '-e .' in requirements:
-            requirements.remove('-e .')
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
-
 setup(
-    name='mlopsproject',
-    version='0.1',
-    author='Vishwas P'
-    author_email='vp14032001@gmail.com'
-    packages=find_packages(),
-    install_requires = get_requirements('requirements.txt')   
+name='MLOPS_Project',
+version='0.0.1',
+author='Vishwas P',
+author_email='vp14032001@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
